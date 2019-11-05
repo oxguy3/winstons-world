cdace8507f9c6c499d1e8657e93805fa0f24b39e
Hayden Schiff <haydenschiff@gmail.com>
2019-11-05 14:24:22 -0500

    basic build system with npm, webpack

---
+++ webpack/base.js
@@ -0,0 +1,52 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader"
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: "file-loader"
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../dist/'),
+    port: 8000
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, "../dist/")
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Kill the Baby?',
+      template: "./src/index.html"
+    })
+  ]
+};
