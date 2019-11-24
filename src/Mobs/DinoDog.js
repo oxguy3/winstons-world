1e13ade2321f3807ef178178dd0bf885fb9276fc
Hayden Schiff <haydenschiff@gmail.com>
2019-11-24 15:14:02 -0500

    DinoDog AI improvements
    will turn around if it hits a wall, and, range can be disabled by setting to -1

--- src/Mobs/DinoDog.js
+++ src/Mobs/DinoDog.js
@@ -28,9 +28,18 @@ export default class DinoDog extends Mob {
   }
 
   getMovementDesires() {
+
+    // turn around if we've left the home zone
     const range = this.getData('range');
-    if (this.x < this.spawnX - range) { this.data.values.goLeft = false; }
-    if (this.x > this.spawnX + range) { this.data.values.goLeft = true; }
+    if (range != -1) {
+      if (this.x < this.spawnX - range) {
+        this.data.values.goLeft = false;
+      }
+      if (this.x > this.spawnX + range) {
+        this.data.values.goLeft = true;
+      }
+    }
+
     return {
       left: this.data.values.goLeft,
       right: !this.data.values.goLeft,
@@ -40,5 +49,17 @@ export default class DinoDog extends Mob {
 
   damage(attacker) {
     super.damage(attacker);
+  }
+
+  onTileCollide(tile) {
+    super.onTileCollide(tile);
+
+    // turn around the AI if we're hitting a wall
+    if (this.body.touching.left || this.body.blocked.left) {
+      this.data.values.goLeft = false;
+    }
+    if (this.body.touching.right || this.body.blocked.right) {
+      this.data.values.goLeft = true;
+    }
   }
 }
