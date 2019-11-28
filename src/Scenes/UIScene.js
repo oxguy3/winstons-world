a64f37dd858ab0e842677a4e5598cd342f793ae2
Hayden Schiff <haydenschiff@gmail.com>
2019-11-28 03:48:18 -0500

    add commander simon animation

--- src/Scenes/UIScene.js
+++ src/Scenes/UIScene.js
@@ -18,14 +18,38 @@ export default class UIScene extends Phaser.Scene {
 
     this.message = '';
 
-    this.messageBox = this.add.image(this.cameras.main.width/2, this.cameras.main.height, 'dialogBox');
-    this.messageBox.setOrigin(0.5, 1).setScale(2).setVisible(false);
+    const camera = this.cameras.main;
+    const fontSize = 32;
+    const margin = 24;
+    const boxStroke = 16;
+    const avatarScale = 4;
+    const avatarSize = 32 * avatarScale;
 
-    const margin = 32;
-    const textY = this.cameras.main.height - this.messageBox.displayHeight + margin;
-    this.messageText = this.add.bitmapText(margin, textY, 'fool', '', 32);
+    // this.messageBox = this.add.image(this.cameras.main.width/2, this.cameras.main.height, 'dialogBox');
+    // this.messageBox.setOrigin(0.5, 1).setScale(2).setVisible(false);
+    this.messageBox = this.add.rectangle(
+      boxStroke/2,
+      camera.height - boxStroke/2,
+      camera.width - avatarSize - margin*2 - boxStroke/2,
+      (fontSize+1)*3 + margin*2 - boxStroke/2,
+      0x3366ff
+    ).setOrigin(0, 1).setVisible(false);
+    this.messageBox.setStrokeStyle(boxStroke, 0x1a3380);
+
+    this.avatar = this.add.sprite(
+      camera.width - margin,
+      camera.height - margin/2,
+      'commander',
+      0
+    );
+    this.avatar.setOrigin(1, 1).setScale(avatarScale).setVisible(false);
+    this.avatar.play('commander_speak');
+
+    const textY = this.messageBox.getBounds().y - boxStroke/2 + margin;
+    this.messageText = this.add.bitmapText(margin, textY, 'fool', '', fontSize);
     this.messageText.setOrigin(0, 0).setVisible(false);
-    this.messageText.setMaxWidth(this.cameras.main.width - margin*2);
+    this.messageText.setMaxWidth(camera.width - margin*3 - avatarSize);
+
   }
 
   update(time, delta) {
@@ -76,6 +100,7 @@ export default class UIScene extends Phaser.Scene {
       const visible = (text != null && text.length != 0);
       this.messageText.setVisible(visible);
       this.messageBox.setVisible(visible);
+      this.avatar.setVisible(visible);
     }
   }
 }
